2f87649b8b68e6e0c259cef9b36e9d99e81cf241
anuja <anuja@joshsoftware.com>
2013-01-21 15:42:08 +0530

    Time format

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ require File.expand_path('../application', __FILE__)
 TimeManagement::Application.initialize!
 
 Time::DATE_FORMATS.merge!(
-	:default => '%m/%d/%Y',
 	:date_time12  => "%m/%d/%Y %I:%M%p",
 	:date_time24  => "%m/%d/%Y %H:%M",
 )
